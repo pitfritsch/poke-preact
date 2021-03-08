ecefe0699167643e0d77d2e40ba74d605dc1d85c
Pedro Fritsch <pedrofritsch98@gmail.com>
2021-03-07 21:25:10 -0300

    initial pokemon listing

--- src/routes/home/index.jsx
+++ src/routes/home/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'preact/hooks';
+import { useEffect, useState } from 'preact/hooks';
 import styled from 'styled-components';
 import Service from '../../utils/services';
 import style from './style.css';
@@ -12,38 +12,68 @@ const Title = styled.h1`
 const Grid = styled.div`
 	display: grid;
 	grid-template-columns: 1fr 1fr 1fr 1fr;
-	grid-gap: 50px;
+	grid-gap: 35px;
+
+	@media (max-width: 1000px) {
+		grid-template-columns: 1fr 1fr;
+	}
+
+	@media (max-width: 400px) {
+		grid-template-columns: 1fr;
+	}
 `
 
-const Card = styled.div`
+const Card = styled.button`
+	outline: none;
+	border: none;
 	min-height: 100px;
 	background-color: #fafafa;
 	border-radius: 5px;
+	font-family: 'Cantarell', sans-serif;
+	color: #222;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	font-size: 20px;
+	text-transform: capitalize;
+	transition: .2s;
+	cursor: pointer;
+	
+	:hover, :focus{
+		background-color: #3b5ca7;
+		color: #ffcb05;
+		transform: scale(1.1);
+	}
+	:active{
+		transform: scale(1);
+	}
 `
 
 const Home = () => {
-	const service = new Service();
+
+	const [pokemons, setPokemons] = useState([])
 
 	useEffect(() => {
-		console.log('montou')
+		const fetchData = async () => {
+			const data = await Service.list()
+			return data
+		}
+		fetchData().then(setPokemons)
 	}, [])
+
+	useEffect(() => {
+		console.log(pokemons)
+	}, [pokemons])
 
 	return (
 		<div class={style.home}>
 			<Title>All Pokémons List</Title>
 			<Grid>
-				<Card onClick={() => service.list()}>
-					1
-				</Card>			
-				<Card>
-					2
-				</Card>			
-				<Card>
-					3
-				</Card>			
-				<Card>
-					4
-				</Card>			
+				{pokemons.map(p => 
+					<Card>
+						{p.name}
+					</Card>			
+				)}		
 			</Grid>		
 		</div>
 	)
--- src/utils/services.js
+++ src/utils/services.js
@@ -1,5 +1,21 @@
 export default class Service {
-  async list() {
-    console.log('oi')
+
+  static get( path ) {
+    const initVar = { 
+      method: 'GET',
+      headers: {},
+      mode: 'cors',
+      cache: 'default' 
+    };
+    return fetch(path, initVar)
+      .then(res => res.json())
+      .then(data => data)
+      .catch(console.error)
+  }
+
+  static async list() {
+    const res = await this.get('https://pokeapi.co/api/v2/pokemon/')
+    // console.log(res.results)
+    return res.results
   }
 }
